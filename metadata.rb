22f629f0fd4a33ff8958876aadc079101027bd9d
kchendil <mkchendil@gmail.com>
2013-04-10 17:23:23 -0400

    readme and metadata changes

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,18 @@
 name             "designer"
 maintainer       "Opscode, Inc."
-maintainer_email "mkchendil@gmail.com"
+maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
-description      "Installs NetIQ Designer"
+description      "Installs/Configures tomcat"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+%w{ java }.each do |cb|
+  depends cb
+end
 
-# %w{ debian ubuntu centos redhat fedora }.each do |os|
-  # supports os
-# end
+%w{ debian ubuntu centos redhat fedora }.each do |os|
+  supports os
+end
 
-recipe "designer::default", "Installs NetIQ Designer"
-
+recipe "tomcat::default", "Installs and configures Tomcat"
+recipe "tomcat::users", "Setup users and roles for Tomcat"
