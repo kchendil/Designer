54e8b07a88b40cf985343d24f761fb65932d1b80
kchendil <kchendil@novell.com>
2014-05-14 11:06:31 +0530

    removed the designer path
    removed the designer path

--- recipes/default.rb
+++ recipes/default.rb
@@ -45,9 +45,7 @@ end
 
 execute "Install Desginer" do
   command " \"#{designer_build_loc}\" -DIA_USER_JRE_HOME=\"#{jre_loc}\" -i silent -f \"/tmp/designer_install.properties\" "
-  creates "/opt/novell/idm/Designer/Designer"
-  not_if { ::File.exists?("/opt/novell/idm/Designer/Designer")}
-  action :run
+   action :run
 end
 
 
